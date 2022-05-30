a7b3e79f65af8b10145136464b61c32548cd99fa
Татьяна <93898987+Tatyshef@users.noreply.github.com>
2022-05-30 09:36:36 +0300

    remove comments from the file

--- 1_hw_python.py
+++ 1_hw_python.py
@@ -1,33 +1,35 @@
 # 1) Создать переменную типа String
-# name = 'Tatyana'
-# print(name, type(name))
+name = 'Tatyana'
+print(name, type(name))
 # 2) Создать переменную типа Integer
-# age = 33
-# print(age, type(age))
+age = 33
+print(age, type(age))
 # 3) Создать переменную типа Float
-# fl_week = 11.55
-# print(fl_week, type(fl_week))
+fl_week = 11.55
+print(fl_week, type(fl_week))
 # 4) Создать переменную типа Bytes
-# b_type = bytes(33)
-# print(b_type, type(b_type))
+b_type = bytes(33)
+print(b_type, type(b_type))
 # 5) Создать переменную типа List
-# list_type = [22, 'Tatyana', 'Mila']
-# print(list_type, type(list_type))
+list_type = [22, 'Tatyana', 'Mila']
+print(list_type, type(list_type))
 
 # 6) Создать переменную типа Tuple
-# tuple = (11, 33),
-# print(tuple, type(tuple)),
+tuple = (11, 33),
+print(tuple, type(tuple))
+
 # 7) Создать переменную типа Set
-# number = {1,22, 34,'Vadim'}
-# print(number, type(number))
+number = {1,22, 34,'Vadim'}
+print(number, type(number))
+
 # 8) Создать переменную типа Frozen set (неизменяемый тип данных, в отличии от set)
-# fs = frozenset([1, 2, 3, 4, 5])
-# print(fs, type(fs))
+fs = frozenset([1, 2, 3, 4, 5])
+print(fs, type(fs))
 
 # 9) Создать переменную типа Dict
-# dict_type = {'key_1': '22',
-#              'key_2': 'Tatyana'}
-# print(dict_type, type(dict_type))
+dict_type = {'key_1': '22',
+             'key_2': 'Tatyana'}
+print(dict_type, type(dict_type))
 
 # 10) Вывести в консоль все выше перечисленные переменные с добавлением типа данных
 # (смотреть выше выполнение).
@@ -36,7 +38,7 @@
 # сканкатенируете эти переменные. Вывести в консоль:
 rep = 'Ruslan'
 per = 'Ludmila'
-vse = 'Ruslan ' + 'Ludmila'
+vse = rep + per
 print(vse)
 
 
@@ -46,9 +48,10 @@ print(vse)
 cor = 'Record'
 roc = 44
 print(cor, str(roc))
+
 # 13) Вывести в одну строку переменные типа String
 # и Integer используя “+” (плюс):
-# pro = 'Toma'
-# pre = 12
-# got = pro + str(12)
-# print(got)
+pro = 'Toma'
+pre = 12
+got = pro + str(12)
+print(got)
